de16eee4ba596d727d8b2ff4fe26d0d71e1fb250
nerellamanoj <nerellamanojkumar@gmail.com>
2024-04-11 20:24:29 +0530

    js files add

---
+++ Day20/script.js
@@ -0,0 +1,7 @@
+// var a=(a,b)=>(a+b);
+// var b=(a,b)=>(a-b);
+// var c=(a,b)=>(a*b);
+// var d=(a,b)=>(a/b);
+
+
+// export {a,b,c,d};
---
+++ Day20/script2.js
@@ -0,0 +1,32 @@
+// import{a,b,c,d} from './script';
+
+// console.log(a(5,3));
+// console.log(b(5,3));
+// console.log(c(5,3));
+// console.log(d(5,3));
+
+//  var a=new Set();
+
+//    a.add("ja");
+//    a.add("ja")
+//    console.log(a);
+//    console.log(a.has("ja"));
+
+
+var a=[1,22,22,77,67,1];
+var f=new Set(a);
+console.log(f);
+
+var h=[...f]
+ 
+console.log(h);
+
+// var a=[1,2,3,4,]
+// var b=[2,56,8]
+//   var c=[...a,...b]
+//   console.log(c);
+
+
+
+
+
